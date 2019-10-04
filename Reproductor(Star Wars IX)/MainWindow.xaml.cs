b2263fd63a716dd32691d634d08fb9a007305b21
susan <susan_rg92@hotmail.com>
2019-10-04 12:07:34 +0200

    Agregar archivos de proyecto.

---
+++ Reproductor(Star Wars IX)/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Reproductor_Star_Wars_IX_
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Trailer1_Checked(object sender, RoutedEventArgs e)
+        {
+            reproductor.Source = new Uri(@"C:\Users\alumno\Desktop\trailer1.mp4");
+        }
+
+        private void Trailer2_Checked(object sender, RoutedEventArgs e)
+        {
+            reproductor.Source = new Uri(@"C:\Users\alumno\Desktop\trailer2.mp4");
+        }
+
+        private void Silenciar_Checked(object sender, RoutedEventArgs e)
+        {
+            reproductor.IsMuted = !reproductor.IsMuted;
+        }
+
+        private void Play_Click(object sender, RoutedEventArgs e)
+        {
+            reproductor.Play();
+        }
+
+        private void Pausa_Click(object sender, RoutedEventArgs e)
+        {
+            reproductor.Pause();
+        }
+
+        private void Stop_Click(object sender, RoutedEventArgs e)
+        {
+            reproductor.Stop();
+        }
+    }
+}
